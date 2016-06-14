b9a322139f8cd8ec00855d55506c848f2a3b3b53
Jerome Goodrich <jeromegoodrich@gmail.com>
2016-06-14 12:18:55 -0500

    first commit

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject httpeek "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0-RC2"]
+                 [ring/ring-mock "0.3.0"]
+                 [ring "1.5.0"]]
+  :profiles {:dev {:dependencies [[speclj "3.3.1"]]}}
+  :plugins [[speclj "3.3.1"]]
+  :test-paths ["spec"])
---
+++ spec/httpeek/core_spec.clj
@@ -0,0 +1,7 @@
+(ns httpeek.core-spec
+  (:require [speclj.core :refer :all]
+            [httpeek.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
