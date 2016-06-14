a0ff7bb37ecc2177f69418ca4fc26eb757e1a072
Jerome Goodrich <jeromegoodrich@gmail.com>
2016-06-14 12:28:16 -0500

    add compojure

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-RC2"]
                  [ring/ring-mock "0.3.0"]
-                 [ring "1.5.0"]]
+                 [ring "1.5.0"]
+                 [compojure "1.5.0"]]
   :profiles {:dev {:dependencies [[speclj "3.3.1"]]}}
   :plugins [[speclj "3.3.1"]]
   :test-paths ["spec"])
