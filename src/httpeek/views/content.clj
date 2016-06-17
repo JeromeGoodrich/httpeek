7af1cbbcfd2c4a7a2e3fdcbfecb60d541d0ebfb2
Jerome Goodrich <jeromegoodrich@gmail.com>
2016-06-17 14:23:43 -0500

    add hiccup templates for page views

---
+++ src/httpeek/views/content.clj
@@ -0,0 +1,22 @@
+(ns httpeek.views.content
+  (require [hiccup.page :as page]
+           [hiccup.form :as form]))
+
+(defn index []
+  (page/html5
+    [:div
+     [:h1 "hello world"]
+     (form/form-to [:post "/bins"]
+        (form/submit-button "create new request bin"))]))
+
+(defn not-found []
+  (page/html5
+    [:div
+     [:h1 "Sorry! The Page You were looking for cannot be found"]]))
+
+(defn inspect []
+  (page/html5
+    [:div
+     [:h1 "hello world"]
+     (form/form-to [:post "/bins"]
+        (form/submit-button "create new request bin"))]))
