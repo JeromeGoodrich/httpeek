ee355b73b40f2c65eb882a3baab9ee7286bb30e9
Jerome Goodrich <jeromegoodrich@gmail.com>
2016-06-17 14:17:29 -0500

    add migratus, jdbc, hiccup, compojure deps

--- project.clj
+++ project.clj
@@ -6,9 +6,21 @@
   :dependencies [[org.clojure/clojure "1.7.0-RC2"]
                  [ring/ring-mock "0.3.0"]
                  [ring "1.5.0"]
-                 [compojure "1.5.0"]]
+                 [compojure "1.5.0"]
+                 [org.clojure/java.jdbc "0.6.1"]
+                 [org.postgresql/postgresql "9.4-1203-jdbc41"]
+                 [hiccup "1.0.5"]
+                 [migratus "0.8.25"]]
   :profiles {:dev {:dependencies [[speclj "3.3.1"]]}}
   :plugins [[speclj "3.3.1"]
-            [lein-ring "0.9.7"]]
+            [lein-ring "0.9.7"]
+            [migratus-lein "0.3.7"]]
   :test-paths ["spec"]
-  :ring {:handler httpeek.handler/app*})
+  :ring {:handler httpeek.handler/app*}
+  :migratus {:store :database
+           :migration-dir "migrations"
+           :db {:classname "com.postgresql.jdbc.Driver"
+                :subprotocol "postgresql"
+                :subname "//localhost:5432/httpeek"
+                :user "httpeek"
+                :password ""}})
