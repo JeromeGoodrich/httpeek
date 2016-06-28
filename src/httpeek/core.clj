d188c96d08f0e6a7e7c598f7dcc49b93b7258f4f
Jerome Goodrich <jeromegoodrich@gmail.com>
2016-06-28 16:37:17 -0500

    add with-exception-handling macro

--- src/httpeek/core.clj
+++ src/httpeek/core.clj
@@ -1,26 +1,26 @@
-(ns httpeek.core
+( httpeek.core
   (:require [httpeek.db :as db]))
+
+(defmacro with-error-handling [default fn]
+  (try
+    fn
+    (catch Exception e
+      default)))
 
 (defn create-bin []
   (db/create-bin))
 
 (defn find-bin-by-id [id]
-  (try
-    (db/find-bin-by-id id)
-    (catch Throwable e
-      nil)))
+  (with-error-handling nil
+    (db/find-bin-by-id id)))
 
 (defn get-requests [bin-id]
-  (try
-    (db/find-requests-by-bin-id bin-id)
-    (catch Throwable e
-      nil)))
+    (with-error-handling []
+      (db/find-requests-by-bin-id bin-id)))
 
 (defn add-request [bin-id full-json-request]
-  (try
-    (db/add-request bin-id full-json-request)
-    (catch Throwable e
-      nil)))
+  (with-error-handling nil
+    (db/add-request bin-id full-json-request)))
 
 (defn all-bins []
   (db/all-bins))
