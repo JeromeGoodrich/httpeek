00c55e85e6b1e29fe05843e5eb1ab2f6db7c5e6f
Jerome Goodrich <jeromegoodrich@gmail.com>
2016-07-01 09:51:36 -0500

    create private bin functionality

--- spec/httpeek/handler_spec.clj
+++ spec/httpeek/handler_spec.clj
@@ -13,13 +13,13 @@
     (it "should return a status of 200"
       (let [response (app* (mock/request :get "/"))]
         (should= 200
-               (:status response)))))
+                 (:status response)))))
 
   (context "Not Found"
     (it "returns a status of 404"
       (let [response (app* (mock/request :get "/foo"))]
         (should= 404
-               (:status response)))))
+                 (:status response)))))
 
   (context "POST /bins"
     (context "a bin is created successfully"
@@ -30,12 +30,90 @@
           (should= (format "/bin/%s?inspect" (:id bin)) (get-in response [:headers "Location"]))))))
 
   (context  "ANY /bin/:id?inspect"
-    (context "GET request with existing bin and no requests"
+    (context "get request with existing bin and no requests"
       (it "returns a 200 status"
-        (let  [bin-id (core/create-bin false)
+        (app* {:cookies {}
+               :remote-addr "localhost"
+               :params {},
+               :route-params {}
+               :headers {"host" "localhost"}
+               :server-port 80
+               :form-params {}
+               :compojure/route [:post "/bins"]
+               :session/key nil
+               :query-params {}
+               :uri "/bins"
+               :server-name "localhost"
+               :query-string nil
+               :scheme :http
+               :request-method :post,
+               :session {}})
+        (let  [bin (->> (core/all-bins) (sort-by :created-at) last)
+               bin-id (:id bin)
                response (app* (mock/request :get (str "/bin/" bin-id) "inspect"))]
           (should= 200 (:status response))
           (should-contain "curl -X" (:body response)))))
+
+    (context "a private bin cannot be accessed by an out-of-session user"
+      (it "returns a 404 status"
+        (app* {:cookies {}
+               :remote-addr "localhost"
+               :params {},
+               :route-params {}
+               :headers {"host" "localhost"}
+               :server-port 80
+               :form-params {"private-bin-checkbox" true}
+               :compojure/route [:post "/bins"]
+               :session/key nil
+               :query-params {}
+               :uri "/bins"
+               :server-name "localhost"
+               :query-string nil
+               :scheme :http
+               :request-method :post
+               :session {}})
+        (let  [bin (->> (core/all-bins) (sort-by :created-at) last)
+               bin-id (:id bin)
+               response (app* (mock/request :get (str "/bin/" bin-id) "inspect"))]
+          (should= 403 (:status response)))))
+
+    (context "a private bin cannot be accessed by an out-of-session user"
+      (it "returns a 404 status"
+        (let [create-bin-response (app* {:cookies {}
+                                         :remote-addr "localhost"
+                                         :params {},
+                                         :route-params {}
+                                         :headers {"host" "localhost"}
+                                         :server-port 80
+                                         :form-params {"private-bin-checkbox" "true"}
+                                         :compojure/route [:post "/bins"]
+                                         :session/key nil
+                                         :query-params {}
+                                         :uri "/bins"
+                                         :server-name "localhost"
+                                         :query-string nil
+                                         :scheme :http
+                                         :request-method :post
+                                         :session {}})
+                                  bin (->> (core/all-bins) (sort-by :created-at) last)
+                                  bin-id (:id bin)
+                                  response (app* {:cookies {"ring-session" {:value "some-key"}}
+                                                  :remote-addr "localhost"
+                                                  :params {},
+                                                  :route-params {}
+                                                  :headers {"host" "localhost"}
+                                                  :server-port 80
+                                                  :compojure/route [:get "/bin/:id"]
+                                                  :session/key nil
+                                                  :query-params {}
+                                                  :uri (str "/bin/" bin-id)
+                                                  :server-name "localhost"
+                                                  :query-string "inspect"
+                                                  :scheme :http
+                                                  :request-method :get
+                                                  :session {:private-bins [bin-id]}})]
+          (should= 403 (:status response)))))
+
 
     (context "GET request with existing bin and request added"
       (it "returns a 200 status"
@@ -50,31 +128,31 @@
         (let  [response (app* (mock/request :get "/bin/not-an-id" "inspect"))]
           (should= 404 (:status response)))))
 
-    (context "Other requests with non-existent bin"
-      (it "returns a 404 status"
-        (let  [response (app* (mock/request :patch (str "/bin/not-an-id") "inspect"))]
-          (should= 404 (:status response))))))
+(context "Other requests with non-existent bin"
+  (it "returns a 404 status"
+    (let  [response (app* (mock/request :patch (str "/bin/not-an-id") "inspect"))]
+      (should= 404 (:status response))))))
 
-  (context "ANY /bin/:id"
-    (context "GET request with existing bin"
-      (it "returns a status of 200"
-        (let [bin-id (core/create-bin false)
-              response (app* (mock/request :get (str "/bin/" bin-id)))]
-          (should= 200 (:status response)))))
+(context "ANY /bin/:id"
+  (context "GET request with existing bin"
+    (it "returns a status of 200"
+      (let [bin-id (core/create-bin false)
+            response (app* (mock/request :get (str "/bin/" bin-id)))]
+        (should= 200 (:status response)))))
 
-    (context "Other request with existing bin"
-      (it "returns a status of 200"
-        (let [bin-id (core/create-bin false)
-              response (app* (mock/request :post (str "/bin/" bin-id)))]
-          (should= 200 (:status response)))))
+  (context "Other request with existing bin"
+    (it "returns a status of 200"
+      (let [bin-id (core/create-bin false)
+            response (app* (mock/request :post (str "/bin/" bin-id)))]
+        (should= 200 (:status response)))))
 
-    (context "GET request with non-existent bin"
-      (it "returns a 404 status"
-        (let  [response (app* (mock/request :get "/bin/not-an-id"))]
-          (should= 404 (:status response)))))
+  (context "GET request with non-existent bin"
+    (it "returns a 404 status"
+      (let  [response (app* (mock/request :get "/bin/not-an-id"))]
+        (should= 404 (:status response)))))
 
-    (context "Other requests with existing bin"
-      (it "returns a 404 status"
-        (let  [response (app* (mock/request :put (str "/bin/not-an-id")))]
-          (should= 404 (:status response)))))))
+  (context "Other requests with existing bin"
+    (it "returns a 404 status"
+      (let  [response (app* (mock/request :put (str "/bin/not-an-id")))]
+        (should= 404 (:status response)))))))
 
--- src/httpeek/handler.clj
+++ src/httpeek/handler.clj
@@ -1,6 +1,8 @@
 (ns httpeek.handler
   (:use compojure.core)
   (:require [ring.util.response :as response]
+            [ring.middleware.params :as middleware]
+            [ring.middleware.session :as session]
             [compojure.route :as route]
             [httpeek.core :as core]
             [cheshire.core :as json]
@@ -8,11 +10,13 @@
 
 (defn- str->uuid [uuid-string]
   (core/with-error-handling nil
-    (java.util.UUID/fromString uuid-string)))
+                            (java.util.UUID/fromString uuid-string)))
 
 (defn- parse-bin-request [request]
+  (prn request)
   (let [id (str->uuid (get-in request [:params :id]))
         inspect (= (:query-string request) "inspect")
+        private-bins (get-in request [:session :private-bins])
         body (json/encode (dissoc request :body))]
     (assoc {} :id id :inspect inspect :body body)))
 
@@ -20,26 +24,33 @@
   (core/add-request id request-body)
   (response/response "ok"))
 
-(defn- set-response-for-bin [id inspect body]
-  (if-let [bin-id (:id (core/find-bin-by-id id))]
-    (if inspect
-      (views/inspect-bin-page bin-id (core/get-requests bin-id))
-      (add-request-to-bin bin-id body))
-    (response/not-found (views/not-found-page))))
+(defn- set-response-for-bin [parsed-request]
+  (let [{:keys [id inspect private-bins body]} parsed-request
+        requested-bin (core/find-bin-by-id id)
+        private? (:private requested-bin)
+        permitted? (some #{id} private-bins)]
+    (if-let [bin-id (:id (core/find-bin-by-id id))]
+      (if inspect
+        (if (and private? (not permitted?))
+          (response/status (response/response "not allowed") 403)
+          (views/inspect-bin-page bin-id (core/get-requests bin-id)))
+        (add-request-to-bin bin-id body))
+    (response/not-found (views/not-found-page)))))
 
 (defn handle-getting-bin [request]
-  (set-response-for-bin (:id request)
-                        (:inspect request)
-                        (:body request)))
+  (set-response-for-bin request))
 
-(defn handle-creating-bin []
-  (->> (core/create-bin)
-    (format "/bin/%s?inspect")
-    (response/redirect)))
+(defn handle-creating-bin [form-params]
+  (let [private? (boolean (get form-params "private-bin-checkbox"))
+        bin-id (core/create-bin private?)]
+    (if private?
+      (-> (response/redirect (format "/bin/%s?inspect" bin-id))
+        (assoc-in [:session] {:private-bins [bin-id]}))
+      (response/redirect (format "/bin/%s?inspect" bin-id)))))
 
 (defroutes app-routes
   (GET "/" [] (views/index-page))
-  (POST "/bins" [] (handle-creating-bin))
+  (POST "/bins" {form-params :form-params} (handle-creating-bin form-params))
   (ANY "/bin/:id" request (-> request
                             (parse-bin-request)
                             (handle-getting-bin)))
@@ -47,4 +58,6 @@
   (route/not-found (views/not-found-page)))
 
 (def app*
-  app-routes)
+  (-> app-routes
+    (middleware/wrap-params)
+    (session/wrap-session)))
