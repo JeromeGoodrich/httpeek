d8ad35a3337bcaa85982c2a30b7f251d9434b3ec
Jerome Goodrich <jeromegoodrich@gmail.com>
2016-07-08 17:02:59 -0500

    use auto-reload? option instead of auto-refresh? option to fix wrap-params issue

--- project.clj
+++ project.clj
@@ -20,6 +20,6 @@
                       [lein-environ "1.0.3"]
                       [lein-ring "0.9.7"]]
             :test-paths ["spec"]
-            :ring {:handler httpeek.handler/app* :auto-refresh? true}
+            :ring {:handler httpeek.handler/web-routes :auto-reload? true}
             :aliases {"migrate" ["run" "-m" "httpeek.migrations/migrate"]
                       "rollback" ["run" "-m" "httpeek.migrations/rollback"]})
